a9ff1ade1a730b75ad80b039951e6164544f93e4
Zack Fair <Zaxx81@gmail.com>
2022-07-21 17:03:56 -0400

    Added import for all hangman images to Hangman.jsx

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -1,4 +1,10 @@
 import hangman_0 from "../assets/hangman_0.png";
+import hangman_1 from "../assets/hangman_1.png";
+import hangman_2 from "../assets/hangman_2.png";
+import hangman_3 from "../assets/hangman_3.png";
+import hangman_4 from "../assets/hangman_4.png";
+import hangman_5 from "../assets/hangman_5.png";
+import hangman_6 from "../assets/hangman_6.png";
 
 function Hangman() {
   let currentImage = hangman_0;
