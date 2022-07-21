d446a8b8788cb4bf1e9cfe070033e7aa2d84ca53
Zack Fair <Zaxx81@gmail.com>
2022-07-21 17:01:39 -0400

    added images assets and updated hangman.jsx

--- src/components/Hangman.jsx
+++ src/components/Hangman.jsx
@@ -1,5 +1,13 @@
+import hangman_0 from "../assets/hangman_0.png";
+
 function Hangman() {
-  return <div className="hangingman mx-auto"></div>;
+  let currentImage = hangman_0;
+
+  return (
+    <div className="hangingman mx-auto">
+      <img src={currentImage} width="256px" height="256px" alt="" />
+    </div>
+  );
 }
 
 export default Hangman;
